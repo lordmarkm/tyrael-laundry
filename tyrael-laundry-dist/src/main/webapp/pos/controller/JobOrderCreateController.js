916daa0290c167a144af57b076a389cfa65fd447
Markm <lordmarkm@gmail.com>
2015-04-08 00:13:16 +0800

    asdf

--- tyrael-laundry-dist/src/main/webapp/pos/controller/JobOrderCreateController.js
+++ tyrael-laundry-dist/src/main/webapp/pos/controller/JobOrderCreateController.js
@@ -1,23 +1,22 @@
 define(function () {
   return ['$scope', '$http', function ($scope, $http) {
 
-  
-  
-  
-  
-  
+    $scope.jobOrder = {
+        serviceType: 'WASH_DRY_FOLD'
+    };
+
     $scope.addresses = [];
     $scope.refreshAddresses = function(address) {
       var params = {address: address, sensor: false};
       return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
-        .then(function(response) {
-          $scope.addresses = response.data.results
-        });
+      .then(function(response) {
+        $scope.addresses = response.data.results
+      });
     };
-  
-  
-  
-  
-  
+
+
+
+
+
   }];
 });
