2e947e761a39dcd0691543ef05551d474594f1c2
Mark Baldwin Martinez <mbmartinez@exist.com>
2015-11-23 11:02:47 +0800

    fix compile errors

--- tyrael-laundry-core/tyrael-laundry-core-service/src/main/java/com/tyrael/laundry/service/BranchService.java
+++ tyrael-laundry-core/tyrael-laundry-core-service/src/main/java/com/tyrael/laundry/service/BranchService.java
@@ -2,6 +2,7 @@ package com.tyrael.laundry.service;
 
 import com.tyrael.commons.data.service.TyraelJpaService;
 import com.tyrael.laundry.model.Branch;
+import com.tyrael.laundry.service.custom.BranchServiceCustom;
 
 public interface BranchService extends BranchServiceCustom,
     TyraelJpaService<Branch> {
